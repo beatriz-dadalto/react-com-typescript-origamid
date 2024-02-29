e9a427c42ff5424f2d5f61c9bddc44da7942a9f8
Beatriz Dadalto <beatrizdadalto@gmail.com>
2024-02-29 20:34:44 -0300

    useFetch TSX 3

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,63 @@ type Produto = {
 };
 
 function App() {
+  const [id, setId] = React.useState("p001");
   const produtos = useFetch<Produto[]>("https://data.origamid.dev/produtos/");
+  const produto = useFetch<Produto>(
+    `https://data.origamid.dev/produtos/${id}`,
+    { cache: "force-cache" },
+  );
 
-  return <div></div>;
+  return (
+    <section className="flex">
+      <div>
+        {produtos.data &&
+          produtos.data.map((produto) => (
+            <button
+              onClick={() => setId(produto.id)}
+              key={produto.id}
+              style={{ fontSize: "0.875rem" }}
+            >
+              {produto.nome}
+            </button>
+          ))}
+      </div>
+      <div>
+        {produto.loading && (
+          <div>
+            <p>Carregando...</p>
+          </div>
+        )}
+        {produto.data && (
+          <ul>
+            <li>
+              <span style={{ fontWeight: "bold" }}>ID:</span> {produto.data.id}
+            </li>
+            <li>
+              <span style={{ fontWeight: "bold" }}>Nome:</span>{" "}
+              {produto.data.nome}
+            </li>
+            <li>
+              <span style={{ fontWeight: "bold" }}>Preço:</span>{" "}
+              {produto.data.preco}
+            </li>
+            <li>
+              <span style={{ fontWeight: "bold" }}>Quantidade:</span>{" "}
+              {produto.data.quantidade}
+            </li>
+            <li>
+              <span style={{ fontWeight: "bold" }}>Descrição:</span>{" "}
+              {produto.data.descricao}
+            </li>
+            <li>
+              <span style={{ fontWeight: "bold" }}>Internacional:</span>{" "}
+              {produto.data.internacional ? "Sim" : "Não"}
+            </li>
+          </ul>
+        )}
+      </div>
+    </section>
+  );
 }
 
 export default App;
