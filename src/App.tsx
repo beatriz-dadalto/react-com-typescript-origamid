f2e2f6407389bd308d4f87efedde942d7f5ffbe9
Beatriz Dadalto <beatrizdadalto@gmail.com>
2024-02-29 18:27:25 -0300

    custom hook TSX

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,29 @@
 import React from "react";
 import videoSrc from "./video.mp4";
+import useLocalStorage from "./useLocalStorage";
 
 function App() {
   const video = React.useRef<HTMLVideoElement>(null);
-  const [playing, setPlaying] = React.useState(false);
+  const [volume, setVolume] = useLocalStorage("volume", "0");
 
   React.useEffect(() => {
-    console.log("teste");
-  }, []);
-
-  function forward() {
     if (!video.current) return;
-    video.current.currentTime += 2;
-  }
-
-  function changePlayBackRate(speed: number) {
-    if (!video.current) return;
-    video.current.playbackRate = speed;
-  }
-
-  function mute() {
-    if (!video.current) return;
-    video.current.muted = !video.current.muted;
-  }
-
-  async function pictureInPicture() {
-    if (!video.current) return;
-    if (document.pictureInPictureElement) {
-      await document.exitPictureInPicture();
-    } else {
-      await video.current.requestPictureInPicture();
+    const volumeStr = Number(volume);
+    if (volumeStr >= 0 && volumeStr <= 1) {
+      video.current.volume = volumeStr;
     }
-  }
+  }, [volume]);
 
   return (
     <div>
       <div className="flex">
-        {playing ? (
-          <button onClick={() => video.current?.pause()}>Pause</button>
-        ) : (
-          <button onClick={() => video.current?.play()}>Play</button>
-        )}
-        <button onClick={forward}>+ 2s</button>
-        <button onClick={() => changePlayBackRate(1)}>1x</button>
-        <button onClick={() => changePlayBackRate(2)}>2x</button>
-        <button onClick={mute}>Mudo</button>
-        <button onClick={pictureInPicture}>PiP</button>
+        <button onClick={() => setVolume("0")}>0</button>
+        <button onClick={() => setVolume("0.5")}>50</button>
+        <button onClick={() => setVolume("1")}>100</button>
       </div>
-      <video
-        controls
-        ref={video}
-        src={videoSrc}
-        onPlay={() => setPlaying(true)}
-        onPause={() => setPlaying(false)}
-      ></video>
+      <video controls ref={video} src={videoSrc}>
+        {" "}
+      </video>
     </div>
   );
 }
