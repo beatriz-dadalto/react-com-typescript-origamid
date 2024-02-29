b67addc406c1d7953ffbe97883646b435f6401ba
Beatriz Dadalto <beatrizdadalto@gmail.com>
2024-02-29 16:55:45 -0300

    useRef TSX 2

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,57 @@ import videoSrc from "./video.mp4";
 
 function App() {
   const video = React.useRef<HTMLVideoElement>(null);
+  const [playing, setPlaying] = React.useState(false);
 
   React.useEffect(() => {
-    console.log(video.current);
+    console.log("teste");
   }, []);
+
+  function forward() {
+    if (!video.current) return;
+    video.current.currentTime += 2;
+  }
+
+  function changePlayBackRate(speed: number) {
+    if (!video.current) return;
+    video.current.playbackRate = speed;
+  }
+
+  function mute() {
+    if (!video.current) return;
+    video.current.muted = !video.current.muted;
+  }
+
+  async function pictureInPicture() {
+    if (!video.current) return;
+    if (document.pictureInPictureElement) {
+      await document.exitPictureInPicture();
+    } else {
+      await video.current.requestPictureInPicture();
+    }
+  }
 
   return (
     <div>
       <div className="flex">
-        <button onClick={() => video.current?.play}>Play</button>
-        <button onClick={() => video.current?.pause}>Pause</button>
+        {playing ? (
+          <button onClick={() => video.current?.pause()}>Pause</button>
+        ) : (
+          <button onClick={() => video.current?.play()}>Play</button>
+        )}
+        <button onClick={forward}>+ 2s</button>
+        <button onClick={() => changePlayBackRate(1)}>1x</button>
+        <button onClick={() => changePlayBackRate(2)}>2x</button>
+        <button onClick={mute}>Mudo</button>
+        <button onClick={pictureInPicture}>PiP</button>
       </div>
-      <video controls ref={video} src={videoSrc}></video>
+      <video
+        controls
+        ref={video}
+        src={videoSrc}
+        onPlay={() => setPlaying(true)}
+        onPause={() => setPlaying(false)}
+      ></video>
     </div>
   );
 }
