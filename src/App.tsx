43c8729aedc8adb63c7ac79e2b0a5c7b2ef34146
Beatriz Dadalto <beatrizdadalto@gmail.com>
2024-03-02 12:01:17 -0300

    Projeto TSX - React Router DOM

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,26 @@ import Sidenav from "./Components/Sidenav";
 import Header from "./Components/Header";
 import { DataContextProvider } from "./Context/DataContext";
 import Vendas from "./Pages/Vendas";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Venda from "./Pages/Venda";
 
 function App() {
   return (
-    <DataContextProvider>
-      <div className="container">
-        <Sidenav />
-        <main>
-          <Header />
-          <Resumo />
-          <Vendas />
-        </main>
-      </div>
-    </DataContextProvider>
+    <BrowserRouter>
+      <DataContextProvider>
+        <div className="container">
+          <Sidenav />
+          <main>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Resumo />} />
+              <Route path="/vendas" element={<Vendas />} />
+              <Route path="/vendas/:id" element={<Venda />} />
+            </Routes>
+          </main>
+        </div>
+      </DataContextProvider>
+    </BrowserRouter>
   );
 }
 
