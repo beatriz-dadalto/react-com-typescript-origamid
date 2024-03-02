46012405e6485915c5ead4b49c06a5c05c4137d2
Beatriz Dadalto <beatrizdadalto@gmail.com>
2024-03-02 10:37:01 -0300

    Projeto TSX - Resumo Vendas

--- src/Pages/Resumo.tsx
+++ src/Pages/Resumo.tsx
@@ -1,12 +1,46 @@
-import React from 'react'
-import { useData } from '../Context/DataContext'
+import { useData } from "../Context/DataContext";
 
 const Resumo = () => {
-  const {data} = useData()
+  const { data } = useData();
+
+  if (data === null) return null;
 
   return (
-    <div>Resumo</div>
-  )
-}
+    <section>
+      <div className="resumo flex mb">
+        <div className="box">
+          <h2>Vendas</h2>
+          <span>
+            {data
+              .filter((item) => item.status !== "falha")
+              .reduce((counter, item) => counter + item.preco, 0)
+              .toLocaleString("pr-BR", { style: "currency", currency: "BRL" })}
+          </span>
+        </div>
+        <div className="box">
+          <h2>Recebido</h2>
+          <span>
+            {data
+              .filter((item) => item.status === "pago")
+              .reduce((counter, item) => counter + item.preco, 0)
+              .toLocaleString("pr-BR", { style: "currency", currency: "BRL" })}
+          </span>
+        </div>
+        <div className="box">
+          <h2>Processando</h2>
+          <span>
+            {data
+              .filter((item) => item.status === "processando")
+              .reduce((counter, item) => counter + item.preco, 0)
+              .toLocaleString("pr-BR", { style: "currency", currency: "BRL" })}
+          </span>
+        </div>
+      </div>
+      <div className="box">
+        Gráficos
+      </div>
+    </section>
+  );
+};
 
-export default Resumo
+export default Resumo;
